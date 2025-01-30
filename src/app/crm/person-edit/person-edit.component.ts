25657b011fa2bf08dd3d7c0e5bf1702555760f48
chrwoizi <christian@woizischke.de>
2025-01-30 01:35:02 +0100

    migrations

--- src/app/crm/person-edit/person-edit.component.ts
+++ src/app/crm/person-edit/person-edit.component.ts
@@ -1,39 +1,33 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Person } from '../../../shared/entities/person';
-import { remult, getValueList, FieldMetadata, FieldValidator } from 'remult';
+import { remult } from 'remult';
 import { FormsModule, NgForm } from '@angular/forms';
-import {
-  ClarityModule,
-  ClrCheckboxModule,
-  ClrComboboxModule,
-  ClrFormsModule,
-  ClrTabsModule,
-} from '@clr/angular';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router'; // Import the Router module
+import { Router, RouterLink } from '@angular/router';
 import { AutofieldComponent } from '../../core/autofield/autofield.component';
 import { EditComponent } from '../../core/edit/edit.component';
 import { AddressEditComponent } from '../address/address-edit.component';
-import { TranslateModule } from '@ngx-translate/core'; // Import TranslateModule
-import { featureFlags } from '../../feature-flags'; // Fix import path
+import { TranslateModule } from '@ngx-translate/core';
+import { featureFlags } from '../../feature-flags';
+import { ButtonModule } from 'primeng/button';
+import { TabViewModule } from 'primeng/tabview';
+import { CardModule } from 'primeng/card';
 
 @Component({
-    selector: 'app-person-edit',
-    imports: [
-        CommonModule,
-        FormsModule,
-        ClrFormsModule,
-        ClarityModule,
-        ClrCheckboxModule,
-        ClrComboboxModule,
-        AutofieldComponent,
-        ClrTabsModule,
-        RouterLink,
-        AddressEditComponent,
-        TranslateModule, // Add TranslateModule to imports
-    ],
-    templateUrl: './person-edit.component.html',
-    styleUrl: './person-edit.component.scss'
+  selector: 'app-person-edit',
+  imports: [
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    TabViewModule,
+    CardModule,
+    AutofieldComponent,
+    RouterLink,
+    AddressEditComponent,
+    TranslateModule,
+  ],
+  templateUrl: './person-edit.component.html',
+  styleUrl: './person-edit.component.scss',
 })
 export class PersonEditComponent extends EditComponent<Person> {
   repo = remult.repo(Person);
@@ -51,7 +45,7 @@ export class PersonEditComponent extends EditComponent<Person> {
     if (this.entity?.addresses?.length == 0) {
       await this.createRelationItem('addresses');
     }
-    this.repo.relations(this.entity!)
+    this.repo.relations(this.entity!);
     this.previewCustomerNumber = await this.entity!.previewCustomerNumber();
   }
 }
