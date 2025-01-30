e5e5318c7b01a52042690999aee9298b944909c8
chrwoizi <christian@woizischke.de>
2025-01-30 01:01:48 +0100

    migrate clarity to primeng

--- src/app/crm/address/address-edit.component.ts
+++ src/app/crm/address/address-edit.component.ts
@@ -14,21 +14,31 @@ import { ClrFormsModule } from '@clr/angular';
 import { FieldsMetadata, getEntityRef } from 'remult';
 import { Address } from '../../../shared/entities/address';
 import { AutofieldComponent } from '../../core/autofield/autofield.component';
-import { EditComponent, RelationFormValidator } from '../../core/edit/edit.component';
+import {
+  EditComponent,
+  RelationFormValidator,
+} from '../../core/edit/edit.component';
 import { Customer } from '../../../shared/entities/customer';
 import { TranslateModule } from '@ngx-translate/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-    selector: 'app-address-edit',
-    imports: [AutofieldComponent, FormsModule, ClrFormsModule, TranslateModule],
-    templateUrl: './address-edit.component.html',
-    styleUrl: './address-edit.component.scss'
+  selector: 'app-address-edit',
+  imports: [
+    AutofieldComponent,
+    FormsModule,
+    ClrFormsModule,
+    TranslateModule,
+    ButtonModule,
+  ],
+  templateUrl: './address-edit.component.html',
+  styleUrl: './address-edit.component.scss',
 })
 export class AddressEditComponent implements OnInit, OnDestroy, AfterViewInit {
   @Input() address!: Address;
   fields!: FieldsMetadata<Address>;
 
-   @Input() parent!: EditComponent<Customer>;
+  @Input() parent!: EditComponent<Customer>;
 
   @ViewChild(NgForm) form!: NgForm;
 
@@ -36,7 +46,8 @@ export class AddressEditComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngOnInit(): void {
     if (this.address) {
-      this.fields = getEntityRef(this.address).metadata.fields as FieldsMetadata<Address>;
+      this.fields = getEntityRef(this.address).metadata
+        .fields as FieldsMetadata<Address>;
     }
   }
 
