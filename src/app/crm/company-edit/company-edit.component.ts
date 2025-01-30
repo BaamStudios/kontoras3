aecb4c5dd58fa22e470361cc3c67423598c0f1ef
chrwoizi <christian@woizischke.de>
2025-01-30 01:12:46 +0100

    migrate clarity to primeng

--- src/app/crm/company-edit/company-edit.component.ts
+++ src/app/crm/company-edit/company-edit.component.ts
@@ -1,42 +1,36 @@
-import { CommonModule, JsonPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
-import {
-  ClarityModule,
-  ClrCheckboxModule,
-  ClrComboboxModule,
-  ClrFormsModule,
-  ClrTabsModule,
-} from '@clr/angular';
-import { TranslateModule } from '@ngx-translate/core'; // Import the TranslateModule
+import { TranslateModule } from '@ngx-translate/core';
+import { ButtonModule } from 'primeng/button';
+import { TabViewModule } from 'primeng/tabview';
 import { remult } from 'remult';
 import { Company } from '../../../shared/entities/company';
 import { AutofieldComponent } from '../../core/autofield/autofield.component';
 import { EditComponent } from '../../core/edit/edit.component';
 import { AddressEditComponent } from '../address/address-edit.component';
-import { Address } from '../../../shared/entities/address';
-import { featureFlags } from '../../feature-flags'; // Corrected import path
+import { featureFlags } from '../../feature-flags';
 
 @Component({
-    selector: 'app-company-edit',
-    imports: [
-        CommonModule,
-        FormsModule,
-        ClrFormsModule,
-        ClarityModule,
-        ClrCheckboxModule,
-        ClrComboboxModule,
-        AutofieldComponent,
-        ClrTabsModule,
-        RouterLink,
-        AddressEditComponent,
-        TranslateModule, // Add TranslateModule to imports
-    ],
-    templateUrl: './company-edit.component.html',
-    styleUrl: './company-edit.component.scss'
+  selector: 'app-company-edit',
+  imports: [
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    TabViewModule,
+    RouterLink,
+    AutofieldComponent,
+    AddressEditComponent,
+    TranslateModule,
+  ],
+  templateUrl: './company-edit.component.html',
+  styleUrl: './company-edit.component.scss',
 })
-export class CompanyEditComponent extends EditComponent<Company> implements OnInit {
+export class CompanyEditComponent
+  extends EditComponent<Company>
+  implements OnInit
+{
   override repo = remult.repo(Company);
 
   previewCustomerNumber: string = '';
@@ -51,7 +45,7 @@ export class CompanyEditComponent extends EditComponent<Company> implements OnIn
     if (this.entity?.addresses?.length == 0) {
       await this.createRelationItem('addresses');
     }
-    this.repo.relations(this.entity!); // Fix the missing closing parenthesis
+    this.repo.relations(this.entity!);
     this.previewCustomerNumber = await this.entity!.previewCustomerNumber();
   }
 }
--- src/app/crm/company-view/company-view.component.ts
+++ src/app/crm/company-view/company-view.component.ts
@@ -4,16 +4,27 @@ import { Company } from '../../../shared/entities/company';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ClarityModule } from '@clr/angular';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { TableModule } from 'primeng/table';
 import { AddressViewComponent } from '../address-view/address-view.component';
-import { TranslateModule } from '@ngx-translate/core'; // Import TranslateModule
+import { TranslateModule } from '@ngx-translate/core';
 import { featureFlags } from '../../feature-flags';
 
 @Component({
-    selector: 'app-company-view',
-    imports: [CommonModule, FormsModule, ClarityModule, RouterLink, AddressViewComponent, TranslateModule], // Add TranslateModule to imports
-    templateUrl: './company-view.component.html',
-    styleUrls: ['./company-view.component.scss']
+  selector: 'app-company-view',
+  imports: [
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    DialogModule,
+    TableModule,
+    RouterLink,
+    AddressViewComponent,
+    TranslateModule,
+  ],
+  templateUrl: './company-view.component.html',
+  styleUrls: ['./company-view.component.scss'],
 })
 export class CompanyViewComponent implements OnInit {
   showConfirmDeleteModal = false;
